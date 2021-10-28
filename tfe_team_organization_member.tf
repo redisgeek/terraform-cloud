6b5e40017604818dea4508cec3819776ef90d8a4
DaShaun <dashaun@dashaun.com>
2021-10-27 19:05:08 -0500

    fmt

--- tfe_team_organization_member.tf
+++ tfe_team_organization_member.tf
@@ -1,9 +1,9 @@
 resource "tfe_team_organization_member" "dashaun" {
-  team_id = tfe_team.owners.id
+  team_id                    = tfe_team.owners.id
   organization_membership_id = tfe_organization_membership.dashaun.id
 }
 
 resource "tfe_team_organization_member" "sean" {
-  team_id = tfe_team.owners.id
+  team_id                    = tfe_team.owners.id
   organization_membership_id = tfe_organization_membership.sean.id
 }
